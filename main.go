bad652251f33e070a34ca74fc3befc02bbf9f7a2
Xavi Ramirez <xavi.ramirez@clever.com>
2016-01-22 22:17:03 +0000

    Reduced the frequency in which cost stats are logged.

--- main.go
+++ main.go
@@ -112,6 +112,7 @@ func initPollClients(wg *sync.WaitGroup) {
 	marathonClient := marathon.NewClient(fmt.Sprintf("%s:%s", marathonMasterHost, marathonMasterPort))
 	mesosClient := mesos.NewClient(fmt.Sprintf("%s:%s", mesosMasterHost, mesosMasterPort))
 
+	num := 0
 	ticker := time.Tick(pollInterval)
 	for _ = range ticker {
 		apps, err := marathonClient.GetApps()
@@ -126,11 +127,14 @@ func initPollClients(wg *sync.WaitGroup) {
 		}
 		mesos.LogState(state)
 
-		costLogger.LogCost(apps.Apps, state)
-		err = pathio.Write(lastRanS3Path, []byte(costLogger.LastRan().Format(time.RFC3339)))
-		if err != nil {
-			log.Fatal(err)
+		if num%10 == 0 {
+			costLogger.LogCost(apps.Apps, state)
+			err = pathio.Write(lastRanS3Path, []byte(costLogger.LastRan().Format(time.RFC3339)))
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
+		num++
 	}
 
 	wg.Done()
